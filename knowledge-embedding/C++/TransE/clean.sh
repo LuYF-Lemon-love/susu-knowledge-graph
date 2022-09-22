a8f8c6b50e0df8ed88c4dc552be7e672ae362444
LuYF-Lemon-love <3555028709@qq.com>
2022-09-22 15:59:34 +0800

    提交代码

---
+++ knowledge-embedding/C++/TransE/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf entity2vec01.vec relation2vec01.vec test_transE transE
---
+++ knowledge-embedding/C++/TransE/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# git clone https://github.com/thunlp/Fast-TransX.git
+# cd Fast-TransX/
+# cd transE/
+
+# train
+g++ transE.cpp -o transE -pthread -O3 -march=native
+./transE -size 50 -input ../data/FB15K/ -output ./ -thread 32 -epochs 1000 -nbatches 1 -alpha 0.01 -margin 1 -note 01
+ 
+# test
+g++ test_transE.cpp -o test_transE -pthread -O3 -march=native
+./test_transE -size 50 -sizeR 50 -input ../data/FB15K/ -init ./ -thread 32 -note 01
